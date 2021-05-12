1d54dcc0b30c4bbb743fd9e618628764c81f1f28
Edwin Samir Patiño G <edwin@tutete.com>
2021-05-12 21:52:40 +0200

    se agrega loterias al reporte de resultados

--- resources/views/reportes/partials/resultados.blade.php
+++ resources/views/reportes/partials/resultados.blade.php
@@ -1,9 +1,9 @@
  <div class="card">
     <div class="card-body">
         <div class="row">
-            {{-- <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="col-xs-12 col-sm-12 col-md-4">
                 @include('reportes.partials.__loterias')
-            </div> --}}
+            </div>
             <div class="col-xs-12 col-sm-12 col-md-4">
                  @include('reportes.partials.__fechas')
             </div>
