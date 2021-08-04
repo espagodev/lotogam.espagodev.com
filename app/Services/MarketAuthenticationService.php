354519aec45f04e5b36bb82a82a7be51079e1e36
Edwin Patiño <espagodev@gmail.com>
2021-08-04 19:29:02 +0200

    cambio

--- app/Services/MarketAuthenticationService.php
+++ app/Services/MarketAuthenticationService.php
@@ -3,7 +3,7 @@
 namespace App\Services;
 
 use App\Traits\ConsumesExternalServices;
-use App\Traits\InteractsWithMarketResponses;
+use App\Traits\InteractsWithmarketResponses;
 
 class MarketAuthenticationService
 {
